c5ed5beec620ec427229e17d6b4fa44e50950040
Gaurav Pradeep <gauravpradeep2004@gmail.com>
2025-07-10 01:16:31 +0530

    adding training setup for unified+instance unfrozen; need a code review

--- model/build.py
+++ model/build.py
@@ -3,5 +3,8 @@ from fvcore.common.registry import Registry
 MODEL_REGISTRY = Registry("model")
 
 def build_model(cfg):
-    model = MODEL_REGISTRY.get(cfg.model.name)(cfg.model, cfg.task.get(cfg.task.name).modalities)
+    if 'unified' in cfg.model.name.lower():
+        model = MODEL_REGISTRY.get(cfg.model.name)(cfg.model, cfg.task.get(cfg.task.name).modalities, cfg.task.get(cfg.task.name).freeze_object_enc)
+    else:
+        model = MODEL_REGISTRY.get(cfg.model.name)(cfg.model, cfg.task.get(cfg.task.name).modalities)
     return model
